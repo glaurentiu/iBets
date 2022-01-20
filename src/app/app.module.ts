968e0405cbd232082118ee660e18188a0ad2506c
Alex G <g.laurentiu@outlook.com>
2022-01-20 18:45:02 +0200

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatCardModule } from '@angular/material/card';
 import {MatDialogModule } from '@angular/material/dialog';
 import{ MatIconModule } from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AuthenticatorComponent } from './tools/authenticator/authenticator.component';
 import { EmailVerificationComponent } from './pages/email-verification/email-verification.component';
@@ -36,6 +37,7 @@ import { ReplyComponent } from './tools/reply/reply.component';
     MatBottomSheetModule,
     MatDialogModule,
     MatIconModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
